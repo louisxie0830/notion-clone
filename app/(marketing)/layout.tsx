a516e062d0fa4d038050fbc2aff1f15d276daf85
Nil Xie <bfgh2678@gmail.com>
2024-08-12 00:08:23 +0800

    init project

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,9 @@
+const MarketingLayout = () => {
+    return (
+        <div className="h-full dark:bg-[#1F1F1F]">
+            <main className="h-full pt-40"></main>
+        </div>
+)   ;
+}
+
+export default MarketingLayout;
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const autoConfig = {
+    providers: [
+        {
+            domain: "https://teaching-beetle-60.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default autoConfig;
